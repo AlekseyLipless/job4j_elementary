55c45f79b10907d269cec40fc60c5a5a23e39f7c
Aleksey <123456789-August>
2024-02-24 20:56:10 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        for (int i = 2, j = 1; i <= 9 ; i++) {
+        for (int i = 2, j = 1; i <= 9; i++) {
             System.out.println(j + " * " + i + " = " + (i * j));
         }
     }
